49789aa37158355daae823b44adb5bc812491cc7
Bernardo Chaves <opingow@gmail.com>
2013-02-24 23:43:29 -0300

    Renaming pattern and starting_at keys

--- jquery.simpleli.js
+++ jquery.simpleli.js
@@ -3,7 +3,7 @@
     
     var settings = $.extend( {
       each: false,
-      starting_at: 0
+      start_at: 0
     }, options);
 
     var methods = {
@@ -16,15 +16,15 @@
       },
 
       all: function() {
-        return this.find('li').slice(settings.starting_at);
+        return this.find('li').slice(settings.start_at);
       },
 
       every: function(quantity) {
-        return this.find('li:nth-child(' + quantity + 'n + ' + settings.starting_at + ')'); 
+        return this.find('li:nth-child(' + quantity + 'n + ' + settings.start_at + ')'); 
       },
 
       first: function(quantity) {
-        return this.find('li').slice(settings.starting_at, settings.starting_at + quantity);
+        return this.find('li').slice(settings.start_at, settings.start_at + quantity);
       },
 
       last: function(quantity) {
@@ -40,12 +40,12 @@
     return this.each(function() {
       var $this = $(this);
 
-      if(settings.hasOwnProperty("pattern")) {
-        var contents = settings.pattern.split('-'),
-            pattern = contents[0],
+      if(settings.hasOwnProperty("option")) {
+        var contents = settings.option.split('-'),
+            option = contents[0],
             quantity = parseInt(contents[1], 10), //NaN nos metodos 'even', 'odd' e 'all'
 
-            lis = methods[pattern].call($this, quantity); 
+            lis = methods[option].call($this, quantity); 
 
         if(settings.each) {
           $.each(lis, function(index, li) {
